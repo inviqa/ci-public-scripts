2faf06f7eb3e84ab9e53f2975cf7c607ef86b2e0
Kieren Evans <kevans@inviqa.com>
2016-02-22 11:23:11 +0000

    Run shellcheck on added scripts.

--- install_mailcatcher.sh
+++ install_mailcatcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Push mailcatcher php ini config
-echo 'sendmail_path = '$(pwd)$'/bin/catchmail\nsmtp_port = 1025' | tee ~/.phpenv/versions/$(phpenv global)/etc/conf.d/sendmail.ini
+printf "sendmail_path = %s/bin/catchmail\nsmtp_port = 1025\n" "$(pwd)" >> "$HOME/.phpenv/versions/$(phpenv global)/etc/conf.d/sendmail.ini"
 # Install mailcatcher gem into bin/, caching gem install into .gems
 mkdir bin .gems
 gem install mailcatcher -v '~> 0.6.1' --no-rdoc --no-ri -i '.gems' -n 'bin'
